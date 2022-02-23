7e3646ecb203dd8403a92a5e550ebd57bce490ed
Morgan Ethier <morganethier@gmail.com>
2022-02-23 15:41:43 -0500

    Imported Scripts from test project, began character movement

---
+++ Assets/Assets/[Scripts]/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public bool cursorActive = true;
+    private void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void EnableCursor(bool enable)
+    {
+        if (enable)
+        {
+            cursorActive = true;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            cursorActive = false;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    private void OnEnable()
+    {
+        AppEvents.MouseCursorEnabled += EnableCursor;
+    }
+
+    private void OnDisable()
+    {
+        AppEvents.MouseCursorEnabled -= EnableCursor;
+    }
+}
