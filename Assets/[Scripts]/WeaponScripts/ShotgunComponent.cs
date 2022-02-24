01e262f4651c0704f4d24174031c00ac883081a5
Morgan Ethier <morganethier@gmail.com>
2022-02-23 20:40:52 -0500

    Designed level

---
+++ Assets/[Scripts]/WeaponScripts/ShotgunComponent.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunComponent : WeaponComponent
+{
+    protected override void FireWeapon()
+    {
+        Vector3 hitLocation;
+
+        if (weaponStats.bulletsInClip > 0 && !isReloading && !weaponHolder.playerController.isRunning)
+        {
+            base.FireWeapon();
+            if (firingEffect)
+            {
+                firingEffect.Play();
+            }
+
+            Ray screenRay = mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+            if (Physics.Raycast(screenRay, out RaycastHit hit, weaponStats.fireDistance, weaponStats.weaponHitLayers))
+            {
+                hitLocation = hit.point;
+
+                Vector3 hitDirection = hit.point - mainCamera.transform.position;
+
+                Debug.DrawRay(mainCamera.transform.position, hitDirection.normalized * weaponStats.fireDistance, Color.red, 1);
+            }
+
+            print("Bullet count: " + weaponStats.bulletsInClip);
+
+        }
+        else if (weaponStats.bulletsInClip <= 0)
+        {
+            weaponHolder.StartReloading();
+        }
+    }
+}
--- Assets/[Scripts]/WeaponScripts/WeaponComponent.cs
+++ Assets/[Scripts]/WeaponScripts/WeaponComponent.cs
@@ -6,6 +6,7 @@ public enum WeaponType
 {
     None,
     Pistol,
+    Shotgun,
     MachineGun
 }
 
